7e40adcd7bb7e4a01ac7856298b2f2e1b5f75c7e
Kishan Mandora <kishanmandora7@gmail.com>
2024-05-26 18:44:49 +0530

    feat: add search query filter

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,12 +1,16 @@
 import { Search as SearchIcon } from "lucide-react";
 import { Input } from "./ui/input";
 
-function Search() {
+function Search({ setSearch }) {
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="sticky top-[53px] bg-zinc-950 z-10">
       <div className="pt-[27px] relative">
         <SearchIcon className="absolute left-2 top-9 h-4 w-4 text-muted-foreground" />
-        <Input placeholder="Search" className="pl-8" />
+        <Input placeholder="Search" className="pl-8" onChange={handleSearch} />
       </div>
     </div>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import Filters from "../components/filters/filters";
 import Search from "../components/search";
 import StudentCard from "../components/student-card";
 import { data } from "../utils/data";
 
 function Home() {
+  const [students, setStudents] = useState(data);
+  const [search, setSearch] = useState("");
+
+  const searchFilteredData = students.filter((student) => {
+    return student.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <div className="flex w-3/4 mx-auto gap-4">
       <Filters />
       <div className="w-3/4">
-        <Search />
+        <Search setSearch={setSearch} />
         <div className="flex flex-col gap-2 mt-4 pb-8">
-          {data.map((student) => {
+          {searchFilteredData.map((student) => {
             return <StudentCard key={student.id} student={student} />;
           })}
         </div>
